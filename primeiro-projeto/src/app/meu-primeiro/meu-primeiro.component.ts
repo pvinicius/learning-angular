ef0ed805c5513de7864699500f9b1799e946b593
Pedro Araujo Correia <peviniciusac@gmail.com>
2018-08-09 09:29:26 -0300

    primeira aula de angular

---
+++ primeiro-projeto/src/app/meu-primeiro/meu-primeiro.component.ts
@@ -0,0 +1,8 @@
+import { Component } from "@angular/core";
+
+@Component({
+    selector:'meu-primeiro-component',// nome da tag html
+    template:`<p>Meu primeiro component angular!</p>` 
+})
+
+export class MeuPrimeiroComponent{ }
